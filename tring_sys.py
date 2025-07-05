ead933a1c31ceb5a78bcf8d06376b2d730d7fd1f
silenttS0ul <abdullahsharhabeel93@gmail.com>
2025-07-05 20:12:05 +0300

    delet some fils

--- tring_sys.py
+++
@@ -1,7 +0,0 @@
-import requests
-import sys
-if len(sys.argv) != 2:
-    sys.exit()
-response = requests.get ("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
-print(requests.json())
-
